6b200cd239652e0abb86b20aef2046823797b5de
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-10-29 21:16:35 -0400

    updated @import to @use, updated vite.config.js to allow for this change along with file naming updates for @use

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./App.scss";
-import "./styles/partials/_global.scss";
+// import "./styles/partials/_global.scss" as *;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
