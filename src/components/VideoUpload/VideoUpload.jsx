2bbab5b5ab1bce94a8e0cc0d72df1a1ec3c5b3d7
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-10-30 23:42:54 -0400

    flexbox styling for upload page applied across devices, removed borderstyling from testing

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -6,63 +6,68 @@ import "./VideoUpload.scss";
 export default function VideoUpload() {
   return (
     <div className="upload-video">
-      <h1 className="upload-video__header">Upload Video</h1>
-      <div className="upload-video__form">
-        <div className="upload-video__thumbnail">
-          {/* Video Thumbnail */}
-          <div className="upload-video__field">
-            <label htmlFor="thumbnail" className="upload-video__label label">
-              <h2>Video thumbnail</h2>
-            </label>
-            <div className="upload-video__thumbnail-image">
-              <img src={uploadThumbnail} alt="Video thumbnail placeholder" />
+      <form className="upload-video__form">
+        <h1 className="upload-video__header">Upload Video</h1>
+        <div className="upload-video__form-container">
+          <div className="upload-video__thumbnail">
+            {/* Video Thumbnail */}
+            <div className="upload-video__field">
+              <label htmlFor="thumbnail" className="upload-video__label label">
+                <h2>Video thumbnail</h2>
+              </label>
+              <div className="upload-video__thumbnail-image">
+                <img src={uploadThumbnail} alt="Video thumbnail placeholder" />
+              </div>
+            </div>{" "}
+          </div>
+          <div className="upload-video__fields">
+            {/* Video Title */}
+            <div className="upload-video__field">
+              <label htmlFor="title" className="upload-video__label label">
+                <h2>Title your video</h2>
+              </label>
+              <input
+                id="title"
+                type="text"
+                className="upload-video__input"
+                placeholder="Add a title to your video"
+                required
+              />
             </div>
-          </div>{" "}
-        </div>
-        <div className="upload-video__fields">
-          {/* Video Title */}
-          <div className="upload-video__field">
-            <label htmlFor="title" className="upload-video__label label">
-              <h2>Title your video</h2>
-            </label>
-            <input
-              id="title"
-              type="text"
-              className="upload-video__input"
-              placeholder="Add a title to your video"
-              required
-            />
-          </div>
-          {/* Video Description */}
-          <div className="upload-video__field">
-            <label htmlFor="description" className="upload-video__label label">
-              <h2>Add a video description</h2>
-            </label>
-            <textarea
-              id="description"
-              className="upload-video__textarea"
-              placeholder="Add a description to your video"
-              rows="4"
-              required
-            />
+            {/* Video Description */}
+            <div className="upload-video__field">
+              <label
+                htmlFor="description"
+                className="upload-video__label label"
+              >
+                <h2>Add a video description</h2>
+              </label>
+              <textarea
+                id="description"
+                className="upload-video__textarea"
+                placeholder="Add a description to your video"
+                rows="4"
+                required
+              />
+            </div>
           </div>
         </div>
-      </div>
-      {/* Buttons */}
-      <div className="upload-video__buttons">
-        <button
-          type="button"
-          className="upload-video__button upload-video__button--publish"
-        >
-          <h4>Publish</h4>
-        </button>
-        <button
-          type="button"
-          className="upload-video__button upload-video__button--cancel"
-        >
-          <h4>Cancel</h4>
-        </button>
-      </div>
+        {/* Buttons */}
+        <div className="upload-video__buttons">
+          <button
+            type="button"
+            className="upload-video__button upload-video__button--publish"
+          >
+            <h4>Publish</h4>
+          </button>
+          <button
+            type="button"
+            className="upload-video__button upload-video__button--cancel"
+          >
+            <h4>Cancel</h4>
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
