c1df925ebdd7eef175504e82a2c809835188b8ff
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-01 19:20:00 -0400

    upload page form submission updated with navigation to home for both publish and cancel links

--- src/components/VideoUpload/VideoUpload.jsx
+++ src/components/VideoUpload/VideoUpload.jsx
@@ -2,14 +2,22 @@
 import React from "react";
 import uploadThumbnail from "../../assets/images/Upload-video-preview.jpg";
 import "./VideoUpload.scss";
-// import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
 export default function VideoUpload() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    alert("Video successfully uploaded!");
+    navigate("/");
+  };
+
   return (
     <div className="upload-video">
-      <form className="upload-video__form">
+      <form className="upload-video__form" onSubmit={handleFormSubmit}>
         <h1 className="upload-video__header">Upload Video</h1>
         <div className="upload-video__form-container">
           <div className="upload-video__thumbnail">
@@ -25,7 +33,7 @@ export default function VideoUpload() {
           </div>
           <div className="upload-video__fields">
             {/* Video Title */}
-            <div className="upload-video__field">
+            <div className="upload-video__field1">
               <label htmlFor="title" className="upload-video__label label">
                 <h2>Title your video</h2>
               </label>
@@ -38,7 +46,7 @@ export default function VideoUpload() {
               />
             </div>
             {/* Video Description */}
-            <div className="upload-video__field">
+            <div className="upload-video__field2">
               <label
                 htmlFor="description"
                 className="upload-video__label label"
@@ -49,7 +57,7 @@ export default function VideoUpload() {
                 id="description"
                 className="upload-video__textarea"
                 placeholder="Add a description to your video"
-                rows="4"
+                // rows="4"
                 required
               />
             </div>
@@ -58,7 +66,7 @@ export default function VideoUpload() {
         {/* Buttons */}
         <div className="upload-video__buttons">
           <button
-            type="button"
+            type="submit"
             className="upload-video__button upload-video__button--publish"
           >
             <h4>Publish</h4>
