2c149b67e6766f5dba4777b3c927d92cd43e6156
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-05 17:36:48 -0500

    server.js inital setup with imports and app.use

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import videoRoutes from "./routes/videos.js";
+import path from "path";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//reserved for static assets rom the public folder, e.g. upload page thumbnail
+app.use("/public", express.static(path.resolve("public")));
+
+//video routes
+app.use("/videos", videoRoutes);
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}`);
+});
