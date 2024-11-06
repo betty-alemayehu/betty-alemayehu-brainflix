82db2e6ab94ce2a5221ee2d915675d70163d3294
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-05 22:58:59 -0500

    added api key to video url allow for playing in browser

--- client/src/components/VideoPlayer/VideoPlayer.jsx
+++ client/src/components/VideoPlayer/VideoPlayer.jsx
@@ -8,13 +8,16 @@ export default function VideoPlayer({ currentVideo }) {
     return <p>Loading video...</p>;
   }
 
+  // Dynamically append the api_key from the environment variable to allow for video playing
+  const videoUrlWithKey = `${video}?api_key=${import.meta.env.VITE_API_KEY}`;
+
   return (
     <div className="video-player">
       {/* Video Section */}
       <div className="video-player__video-container">
         <figure>
           <video className="video-player__video" controls poster={image}>
-            <source src={video} type="video/mp4" />
+            <source src={videoUrlWithKey} type="video/mp4" />
             Some error message here...
           </video>
         </figure>
