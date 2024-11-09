5f991b0efae045169e245b9ca901f7f1cd936ae2
betelhemalemayehu <Betelhem.L.A@gmail.com>
2024-11-09 00:32:48 -0500

    updated folder structure for submission, removed problematic diving deeper additions like image upload and search functionality for MVP ship

---
+++ betty-alemayehu-brainflix/src/components/NavBar/NavBar.jsx
@@ -0,0 +1,77 @@
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import React from "react";
+import "./NavBar.scss";
+// import logo from "../../assets/images/Logo/BrainFlix-logo.svg";
+// import searchIcon from "../../assets/images/Icons/search.svg";
+// import userIcon from "../../assets/images/Mohan-muruge.jpg";
+// import uploadIcon from "../../assets/images/Icons/upload.svg";
+
+const URL = import.meta.env.VITE_API_URL;
+
+export default function NavBar() {
+  const [searchValue, setSearchValue] = useState("");
+  const [isError, setisError] = useState(false);
+
+  const handleInputChange = (error) => {
+    const value = error.target.value;
+    setSearchValue(value);
+
+    setisError(value.trim() === "");
+  };
+
+  return (
+    <header className="nav-bar">
+      {/* Home Icon: inserting link in place of image */}
+      <div className="nav-bar__container">
+        <NavLink to="/" className="nav-bar__logo logo-icon">
+          <img src={`${URL}/Logo/Brainflix-logo.svg`} alt="BrainFlix Logo" />
+        </NavLink>
+        <div
+          className={`nav-bar__search-container ${
+            isError ? "nav-bar__error" : ""
+          }`}
+        >
+          <img
+            src={`${URL}/Icons/search.svg`}
+            alt="Search Icon"
+            className="nav-bar__search-icon"
+          />
+          <input
+            required
+            value={searchValue}
+            onChange={handleInputChange}
+            className="nav-bar__search"
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search"
+          />
+          <img
+            className="nav-bar__user-icon avatar"
+            src={`${URL}/images/Mohan-muruge.jpg`}
+            alt="user icon"
+          />
+        </div>
+
+        {/* Search bar: inserting link in place of image */}
+        <NavLink to="/upload" className="nav-bar__link">
+          <button className="nav-bar__button">
+            <img
+              src={`${URL}/Icons/upload.svg`}
+              alt="Upload Icon"
+              className="nav-bar__button-icon"
+            />
+            <h4 className="nav-bar__button-copy">Upload</h4>
+          </button>
+        </NavLink>
+        {/* User Avatar */}
+        <img
+          className="nav-bar__user-icon--tablet avatar"
+          src={`${URL}/images/Mohan-muruge.jpg`}
+          alt="user icon"
+        />
+      </div>
+    </header>
+  );
+}
---
+++ betty-alemayehu-brainflix/src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+import "./VideoUploadPage.scss";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const URL = import.meta.env.VITE_API_URL;
+export default function VideoUploadPage({ fetchVideos }) {
+  // const { videoId } = useParams();
+
+  useEffect(() => {
+    document.title = "BrainFlix | Upload";
+  }, []);
+  const navigate = useNavigate();
+
+  const [description, setDescription] = useState("");
+  const [titleP, setTitleP] = useState("");
+  const [titleError, setTitleError] = useState(false);
+  const [descriptionError, setDescriptionError] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Check for missing inputs and set error states accordingly
+    const isTitleValid = titleP.trim() !== "";
+    const isDescriptionValid = description.trim() !== "";
+
+    if (isTitleValid && isDescriptionValid) {
+      try {
+        const videoData = {
+          title: titleP,
+          description: description,
+        };
+        axios.post(`${import.meta.env.VITE_API_URL}/videos`, videoData);
+
+        alert("Video successfully uploaded!");
+
+        setTitleP("");
+        setDescription("");
+        setTitleError(false);
+        setDescriptionError(false);
+        fetchVideos();
+        navigate("/");
+      } catch (error) {
+        console.log("Error uploading video: ", error);
+      }
+    } else {
+      setTitleError(!isTitleValid);
+      setDescriptionError(!isDescriptionValid);
+    }
+  };
+
+  const handleDescriptionChange = (event) => {
+    setDescription(event.target.value);
+    if (event.target.value.trim()) {
+      setDescriptionError(false); // Remove error when input is valid
+    }
+  };
+
+  const handleTitlePChange = (event) => {
+    setTitleP(event.target.value);
+    if (event.target.value.trim()) {
+      setTitleError(false); // Remove error when input is valid
+    }
+  };
+  return (
+    <div className="video-upload-page">
+      <main className="video-upload-page__content">
+        <div className="upload-video">
+          <form className="upload-video__form" onSubmit={handleFormSubmit}>
+            <h1 className="upload-video__header">Upload Video</h1>
+            <div className="upload-video__form-container">
+              <div className="upload-video__thumbnail">
+                {/* Video Thumbnail */}
+                <div className="upload-video__field">
+                  <label
+                    htmlFor="thumbnail"
+                    className="upload-video__label label"
+                  >
+                    Video thumbnail
+                  </label>
+                  <div className="upload-video__thumbnail-image">
+                    <img
+                      src={`${URL}/images/sample-thumbnail.jpg`}
+                      alt="Video thumbnail placeholder"
+                    />
+                  </div>
+                </div>{" "}
+              </div>
+              <div className="upload-video__fields">
+                {/* Video Title */}
+                <div className="upload-video__field--title">
+                  <label htmlFor="title" className="upload-video__label label">
+                    Title your video
+                  </label>
+                  <input
+                    id="titleP"
+                    type="titleP"
+                    value={titleP}
+                    onChange={handleTitlePChange}
+                    className={`upload-video__input ${
+                      titleError ? "upload-video__error" : ""
+                    }`}
+                    placeholder="Add a title to your video"
+                    required
+                  />
+                </div>
+                {/* Video Description */}
+                <div className="upload-video__field">
+                  <label
+                    htmlFor="description"
+                    className="upload-video__label label"
+                  >
+                    Add a video description
+                  </label>
+                  <textarea
+                    required
+                    id="description"
+                    name="description"
+                    value={description}
+                    onChange={handleDescriptionChange}
+                    className={`upload-video__textarea ${
+                      descriptionError ? "upload-video__error" : ""
+                    }`}
+                    placeholder="Add a description to your video"
+                    rows="4"
+                  />
+                </div>
+              </div>
+            </div>
+            {/* Buttons */}
+            <div className="upload-video__buttons">
+              <button
+                type="submit"
+                className="upload-video__button upload-video__button--publish"
+              >
+                <img
+                  src={`${URL}/Icons/upload.svg`}
+                  alt="Upload Icon"
+                  className="upload-video__button-icon"
+                />
+                <h4>Publish</h4>
+              </button>
+              <Link
+                to="/"
+                className="upload-video__button upload-video__button--cancel"
+              >
+                Cancel
+              </Link>
+            </div>
+          </form>
+        </div>
+      </main>
+    </div>
+  );
+}
